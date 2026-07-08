e0301b4f50197544cd11ed68c1d69d9faaaef97f
emman <emman@Adrian>
2026-07-08 15:54:41 -0400

    segunda subida

--- src/Vista/LoginVista.java
+++ src/Vista/LoginVista.java
@@ -7,7 +7,7 @@ public class LoginVista extends javax.swing.JFrame {
     public LoginVista() {
         initComponents();
     }
-
+//Holaaaaa
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
